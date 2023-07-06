d6f6ea1d0c6d69915ac8b6866132dbeeaf68f916
ponkotuy <i@ponkotuy.com>
2023-07-07 03:13:51 +0900

    Create Map Generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { MapContainer, Marker, Rectangle, TileLayer, Tooltip, useMapEvents } from "react-leaflet";
+import { LatLng, latLng, Map, icon } from "leaflet";
+import 'leaflet/dist/leaflet.css';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import shadowMarkerIcon from 'leaflet/dist/images/marker-shadow.png'
+
+let DefaultIcon = icon({iconUrl: markerIcon, shadowUrl: shadowMarkerIcon});
+
+interface LocationHandlerProps {
+  onChange: (latLng: LatLng) => void;
+}
+
+function LocationHandler({onChange}: LocationHandlerProps) {
+  useMapEvents({
+    moveend: (e) => {
+      onChange(e.target.getCenter());
+    }
+  });
+  return <></>;
+}
 
 function App() {
+  const [position, setPosition] = useState(latLng(35.6809591, 139.7673068));
+  const [smallMap, setSmallMap] = useState<Map | null>(null);
+  const [largeMap, setLargeMap] = useState<Map | null>(null);
+
+  useEffect(() => {
+    largeMap?.flyTo(position);
+  }, [largeMap, position])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapContainer
+        id='small-map'
+        center={position}
+        zoom={12}
+        scrollWheelZoom={true}
+        zoomControl={false}
+        style={{width: "1280px", height: "960px"}}
+        ref={setSmallMap}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LocationHandler onChange={setPosition} />
+        <Marker position={position} icon={DefaultIcon}>
+          <Tooltip permanent>Tokyo station</Tooltip>
+        </Marker>
+      </MapContainer>
+      <MapContainer
+        id='large-map'
+        center={position}
+        zoom={8}
+        scrollWheelZoom={true}
+        zoomControl={false}
+        style={{width: "480px", height: "320px"}}
+        ref={setLargeMap}
+        attributionControl={false}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {smallMap && <Rectangle bounds={smallMap.getBounds()} />}
+      </MapContainer>
     </div>
   );
 }
