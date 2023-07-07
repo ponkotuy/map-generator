e6060f81e8c0021161f0ec117d98b8cb73dfc82b
ponkotuy <i@ponkotuy.com>
2023-07-08 03:30:38 +0900

    Add JSON form

--- src/App.tsx
+++ src/App.tsx
@@ -21,14 +21,40 @@ function LocationHandler({onChange}: LocationHandlerProps) {
   return <></>;
 }
 
+interface SimpleMarker {
+  lat: number;
+  lng: number;
+  context: string;
+}
+
 function App() {
   const [position, setPosition] = useState(latLng(35.6809591, 139.7673068));
   const [smallMap, setSmallMap] = useState<Map | null>(null);
   const [largeMap, setLargeMap] = useState<Map | null>(null);
+  const [jsonValue, setJsonValue] = useState('');
+  const [markers, setMarkers] = useState<SimpleMarker[]>([]);
 
   useEffect(() => {
     largeMap?.flyTo(position);
-  }, [largeMap, position])
+  }, [largeMap, position]);
+
+  const loadJson = () => {
+    const obj = JSON.parse(jsonValue)
+    if(obj?.center?.lat && obj?.center?.lng) {
+      setPosition(latLng(obj.center.lat, obj.center.lng));
+    }
+    if(obj?.markers) {
+      setMarkers(obj.markers);
+    }
+  }
+
+  const updateJson = () => {
+    const json = JSON.stringify({
+      center: position,
+      markers
+    });
+    setJsonValue(json);
+  }
 
   return (
     <div className="App">
@@ -46,9 +72,11 @@ function App() {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <LocationHandler onChange={setPosition} />
-        <Marker position={position} icon={DefaultIcon}>
-          <Tooltip permanent>Tokyo station</Tooltip>
-        </Marker>
+        {markers.map(marker => (
+          <Marker position={latLng(marker.lat, marker.lng)} icon={DefaultIcon}>
+            <Tooltip permanent>{marker.context}</Tooltip>
+          </Marker>
+        ))}
       </MapContainer>
       <MapContainer
         id='large-map'
@@ -65,6 +93,12 @@ function App() {
         />
         {smallMap && <Rectangle bounds={smallMap.getBounds()} />}
       </MapContainer>
+      <form>
+        <label htmlFor='json'>State JSON</label>
+        <textarea id='json' value={jsonValue} onChange={e => setJsonValue(e.target.value)} />
+        <button type='button' onClick={loadJson}>Load JSON</button>
+        <button type='button' onClick={updateJson}>Update Textarea JSON</button>
+      </form>
     </div>
   );
 }
